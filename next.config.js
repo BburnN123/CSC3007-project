b0494cbb8bd1f8fa8b848425bd1f679dee258d0e
falte111 <jianzhong.heng@gmail.com>
2022-07-07 11:14:25 +0800

    initial commit nextjs config change

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images:          {
-        domains: [ "farm1.staticflickr.com" ],
+  reactStrictMode: true,
+  //     images:          {
+  //         domains: [ "farm1.staticflickr.com" ],
 
-        loader: "akamai",
-        path:   "/CSC3007-project"
-    },
+  //         loader: "akamai",
+  //         path:   "/CSC3007-project"
+  //     },
 
-    basePath:    "/CSC3007-project",
-    assetPrefix: "/CSC3007-project",
+  //     basePath:    "/CSC3007-project",
+  //     assetPrefix: "/CSC3007-project",
 };
-  
+
 module.exports = nextConfig;
