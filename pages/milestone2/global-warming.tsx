b55397e5cb5fcb125e40e1fbf2353416ae210710
Hui Jie <huijie_1995@hotmail.com>
2022-07-17 01:51:46 +0800

    Add title global warming

--- pages/milestone2/global-warming.tsx
+++ pages/milestone2/global-warming.tsx
@@ -2,17 +2,34 @@
 import React from "react";
 
 /* COMPONENTS */
+import Text from "@base/design/text";
 import D3GlobalWarmingChoroplethMap from "@base/components/d3/d3-global-warming-choropleth-map";
 
 class GlobalWarming extends React.PureComponent {
     render(): JSX.Element {
         return (
             <>
+                <style>{`
+                    .ctn-heading{
+                        padding:30px;
+                        text-align:center;
+                    }
+                `}</style>
+                
+                <div className="ctn-heading">
+                    <Text
+                        type="xxl-heading"
+                        color="white"
+                    >
+                        CO<sub>2</sub> Emission
+                    </Text>
+                </div>
+
                 <D3GlobalWarmingChoroplethMap
                     type="map"
-                    drawGraticules={true}
                     size={{ width: 1000, height: 500 }}
                 />
+
 
             </>
         );
